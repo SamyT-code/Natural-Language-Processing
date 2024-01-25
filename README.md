7f6193da315e259e2b6b6035cf8bdea0c5d39ff8
Samy Touabi <stoua026@uottawa.ca>
2024-01-24 22:58:39 -0500

    added some files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Winter 2024 CSI 4107 Group 7 Assignments
-
-Samy Touabi
-Jackie Chen
-Kian Zahrai
---
+++ preprocessor.py
@@ -0,0 +1,7 @@
+# Step1. [10 points]  
+# Preprocessing:  Implement preprocessing functions for tokenization and stopword removal. 
+#                 The index terms will be all the words left after filtering out markup that is not part of 
+#                 the text, punctuation tokens, numbers, stopwords, etc. Optionally, you can use the Porter stemmer 
+#                 to stem the index words. 
+# • Input: Documents that are read one by one from the collection
+# • Output: Tokens to be added to the index (vocabulary)
