1a50d7ceb4969875e7e694607fdae605bf2d990a
jchan16 <jackiechen20@yahoo.ca>
2024-02-09 14:11:47 -0500

    Removed more files

--- tester.py
+++
@@ -1,9 +0,0 @@
-from vocabvector import Vocabvector
-
-#Vocabvector.setWords(["hi","hello","bye"])
-
-v1 = Vocabvector([0,0,2])
-v2 = Vocabvector([2,3,5])
-
-
-print(Vocabvector.cosineSimilarity(v1,v2))
